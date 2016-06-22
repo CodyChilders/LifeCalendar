8be9eec3f77c283359f1114e92d88af82f82e3b9
CodyIsAwesome <cchilder@ucsc.edu>
2016-06-21 19:06:30 -0700

    Added broken cell generation code
    The commented out for loops are too inefficient to run correctly, and
    the cell layout coloring is acting funny.

--- LifeCalendar/LifeCalendar/Form1.cs
+++ LifeCalendar/LifeCalendar/Form1.cs
@@ -15,6 +15,22 @@ namespace LifeCalendar
         public Form1()
         {
             InitializeComponent();
+            InitializeButtons();
+        }
+
+        private void InitializeButtons()
+        {
+            /*
+            for(int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                for(int j = 0; j < tableLayoutPanel1.ColumnCount; j++)
+                {
+                    tableLayoutPanel1.Controls.Add(new Button(), i, j);
+                    Console.WriteLine("{0} {1}", i, j);
+                }
+            }
+             * */
+            tableLayoutPanel1.CellBorderStyle = TableLayoutPanelCellBorderStyle.Single;
         }
 
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
@@ -25,6 +41,14 @@ namespace LifeCalendar
         private void doStuffToolStripMenuItem_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello!");
+        }
+
+        private void tableLayoutPanel1_CellPaint(object sender, TableLayoutCellPaintEventArgs e)
+        {
+            if ((e.Column + e.Row) % 2 == 1)
+                e.Graphics.FillRectangle(Brushes.Black, e.CellBounds);
+            else
+                e.Graphics.FillRectangle(Brushes.White, e.CellBounds);
         }
     }
 }
